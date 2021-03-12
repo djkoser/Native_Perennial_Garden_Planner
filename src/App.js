6391c10c5021c0711e3a69c3b2079de3d921e179
djkoser <koser.david@gmail.com>
2021-03-11 22:30:52 -0600

    Initial commit post-installation

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  constructor {
+    super(props);
+  }
+  return (
+    <div className="App">
+    </div>
+  );
+}; 
+
+
+
---
+++ src/Components/PlantFigure.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function PlantFigure (props) {
+  
+  return (
+
+  );
+};
