b0a52764267feb5df6d0f748ee38af22fe33d060
djkoser <koser.david@gmail.com>
2021-03-15 09:23:11 -0500

    Reconfigure Add Notes button for improved usability, add logic to hide "Notes: " unless project_notes present

--- src/Components/AddedPlant.js
+++ src/Components/AddedPlant.js
@@ -12,8 +12,7 @@ export default function AddedPlant (props) {
       return (
       <li key={`myPlant${el.id}`}>
       <img className={el.id} onClick={ev=> removeFromMyPlants(ev.target.className,bloomTime)} alt={`${el.botanical_name}, commonly known as ${el.common_name}.`} src={el.src} width='50px'/>
-      <button className={el.id} onClick={ev=>toggleEdit(ev.target.className)}>Add Notes</button>
-      <span>Common Name:{el.common_name}, Botanical Name:{el.botanical_name}, Moisture:{el.moisture}, Sun:{el.sun}, Height:{el.height}, Bloom Time:{el.bloom_time}, Notes: {editToggle.toggle && editToggle.id ===el.id.toString() ? (<textarea onChange={ev=> handleChange(ev.target.value)} value={notesInput}></textarea>) : el.project_notes}
+      <span>Common Name:{el.common_name}, Botanical Name:{el.botanical_name}, Moisture:{el.moisture}, Sun:{el.sun}, Height:{el.height}, Bloom Time:{el.bloom_time} <button className={el.id} onClick={ev=>toggleEdit(ev.target.className)}>Add Notes</button>{el.project_notes ? (<span>Notes: </span>) : null}{editToggle.toggle && editToggle.id ===el.id.toString() ? (<textarea onChange={ev=> handleChange(ev.target.value)} value={notesInput}></textarea>) : el.project_notes} 
       </span>
       <br/>
       </li>
