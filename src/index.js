910812c1b888b938da7f30bd2f17827892412981
djkoser <koser.david@gmail.com>
2021-03-15 15:02:46 -0500

    Move print layout to seperate CSS file, add CSS transition to img tags

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import '../node_modules/react-toastify/dist/ReactToastify.css';
+import './printLayout.css'
 
 ReactDOM.render(
   <React.StrictMode>
